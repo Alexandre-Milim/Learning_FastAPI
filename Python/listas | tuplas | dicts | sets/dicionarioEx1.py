48555d3160547216efd146e103986a410d630bc2
Alexandre-Milim <alexandremilim15@gmail.com>
2025-12-13 20:27:10 -0300

    dict

---
+++ Python/listas | tuplas | dicts | sets/dicionarioEx1.py
@@ -0,0 +1,13 @@
+#Crie um dicionário com 3 contatos (nome: telefone). Adicione um novo contato e remova um existente.
+
+contatos = {
+    "João": "11-9999-1234",
+    "Maria": "11-8888-5678",
+    "Pedro": "11-7777-9012"
+}
+
+contatos.pop("João")
+
+contatos ["Alexandre"] = "18-72845-4567"
+
+print(contatos)
